d8986a4edad04db7bd30555c3895a47a83ef204e
VcatYuki <neusbosk@gmail.com>
2021-11-04 15:40:10 +0100

    Interestellar web model

---
+++ javascript/showhidden.js
@@ -0,0 +1,13 @@
+console.log('Hello World')
+
+document.querySelector('#hamburguesa').onclick = showhidden
+
+function showhidden () {
+    document.querySelector('nav').classList.toggle('show');   
+}
+
+
+//function name(parameter) {}
+    
+
+
